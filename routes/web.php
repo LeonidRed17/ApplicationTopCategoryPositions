fca33af00ea6c45388ab855ffebdf30f54c2af3c
leonidRed17 <leograves138@gmail.com>
2025-02-14 18:28:48 +0300

    Create console command, added started migration, make model to work with insertiing data

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,36 @@
 <?php
+/*
+- 
 
+Роут для получения данных
+- Подключаться к API (Минимальная дата когда есть данные 2025-01-15)
+- парсить необходимые данные
+- вносить в бд 
+
++Получать 1 параметр в гет-роуте apptopcatergory{date}
+- выводить из БД значения по дате в какой категории и какое место приложение занимало в рейтинге
+
+Дополнительно
+- Добавить ограничение по количеству запросов на endpoint с одного ip-адреса: 5 запросов в минуту.
+- Добавить логирование запросов на endpoint. 
+*/
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\TopCategoryController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/appTopCategory/{date}/{ass}', function ($date) {
+/*
+Route::get('/appTopCategory/{date}/', function ($date) {
     return view('test',['date' => $date]);
 });
+*/
+
+Route::get('/appTopCategory',[MainController::class, "getAppticaTopHistoryData"]);
+
+
+Route::get('/getAppTop',[TopCategoryController::class, "getAppTop"]);
+
